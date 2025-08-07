dbc71578c15005a3f0f45e0706959dabe0aebd27
Your Name <your.email@example.com>
2025-08-07 16:05:28 +0300

    Add Post model and blog list view

--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -1,4 +1,10 @@
+# blog/views.py
 from django.http import HttpResponse
+from .models import Post
 
 def home(request):
-    return HttpResponse("Йо, это мой блог!")
+    posts = Post.objects.all()
+    post_list = "<h1>Мой блог</h1>"
+    for post in posts:
+        post_list += f"<h2>{post.title}</h2><p>{post.content}</p><p>Создано: {post.created_at}</p>"
+    return HttpResponse(post_list)
